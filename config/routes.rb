b4403293f78424d64a65dcf2bca45bf8975c9b3f
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-06-27 13:34:17 -0400

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  resources :users, only: [:new, :create]
+
+  get 'login', to: 'sessions#new', as: 'new_session'
+  post 'login', to: 'sessions#create', as: 'session'
+  delete 'logout', to: 'sessions#destroy', as: 'destroy_session'
+
+
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
